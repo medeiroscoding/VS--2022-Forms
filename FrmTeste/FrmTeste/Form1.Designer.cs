f0c14c7c31b51cac047f9229e8face4a0e14b379
medeiroscoding <eramarketing2009@gmail.com>
2025-10-21 12:38:12 -0300

    Add files via upload

---
+++ FrmTeste/FrmTeste/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace FrmTeste
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            panelConfig = new Panel();
+            label6 = new Label();
+            pictureBox6 = new PictureBox();
+            panelLocal = new Panel();
+            label4 = new Label();
+            pictureBox4 = new PictureBox();
+            panelLoja = new Panel();
+            label3 = new Label();
+            pictureBox3 = new PictureBox();
+            panelClientes = new Panel();
+            label2 = new Label();
+            pictureBox2 = new PictureBox();
+            panelHome = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel3 = new Panel();
+            pictureBox5 = new PictureBox();
+            label5 = new Label();
+            panel2 = new Panel();
+            panel1.SuspendLayout();
+            panelConfig.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            panelLocal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            panelLoja.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panelClientes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panelHome.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Indigo;
+            panel1.Controls.Add(panelConfig);
+            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(panelLocal);
+            panel1.Controls.Add(panelLoja);
+            panel1.Controls.Add(panelClientes);
+            panel1.Controls.Add(panelHome);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(196, 556);
+            panel1.TabIndex = 0;
+            // 
+            // panelConfig
+            // 
+            panelConfig.BackColor = SystemColors.Info;
+            panelConfig.Controls.Add(label6);
+            panelConfig.Controls.Add(pictureBox6);
+            panelConfig.Location = new Point(0, 493);
+            panelConfig.Name = "panelConfig";
+            panelConfig.Size = new Size(196, 60);
+            panelConfig.TabIndex = 3;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(58, 8);
+            label6.Name = "label6";
+            label6.Size = new Size(102, 28);
+            label6.TabIndex = 2;
+            label6.Text = "Definições";
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(12, 8);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(40, 40);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 2;
+            pictureBox6.TabStop = false;
+            pictureBox6.Click += pictureBox6_Click;
+            // 
+            // panelLocal
+            // 
+            panelLocal.BackColor = Color.Indigo;
+            panelLocal.Controls.Add(label4);
+            panelLocal.Controls.Add(pictureBox4);
+            panelLocal.Location = new Point(0, 292);
+            panelLocal.Name = "panelLocal";
+            panelLocal.Size = new Size(196, 87);
+            panelLocal.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(75, 22);
+            label4.Name = "label4";
+            label4.Size = new Size(67, 31);
+            label4.TabIndex = 1;
+            label4.Text = "Local";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(12, 22);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(57, 48);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 0;
+            pictureBox4.TabStop = false;
+            // 
+            // panelLoja
+            // 
+            panelLoja.BackColor = Color.Indigo;
+            panelLoja.Controls.Add(label3);
+            panelLoja.Controls.Add(pictureBox3);
+            panelLoja.Location = new Point(0, 199);
+            panelLoja.Name = "panelLoja";
+            panelLoja.Size = new Size(196, 87);
+            panelLoja.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(75, 22);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 31);
+            label3.TabIndex = 1;
+            label3.Text = "Loja";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(12, 22);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(57, 48);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 0;
+            pictureBox3.TabStop = false;
+            // 
+            // panelClientes
+            // 
+            panelClientes.BackColor = Color.Indigo;
+            panelClientes.Controls.Add(label2);
+            panelClientes.Controls.Add(pictureBox2);
+            panelClientes.Location = new Point(0, 106);
+            panelClientes.Name = "panelClientes";
+            panelClientes.Size = new Size(196, 87);
+            panelClientes.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(75, 22);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 31);
+            label2.TabIndex = 1;
+            label2.Text = "Clientes";
+            label2.Click += label2_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(12, 22);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(57, 48);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // panelHome
+            // 
+            panelHome.BackColor = SystemColors.Info;
+            panelHome.Controls.Add(label1);
+            panelHome.Controls.Add(pictureBox1);
+            panelHome.Location = new Point(0, 0);
+            panelHome.Name = "panelHome";
+            panelHome.Size = new Size(196, 100);
+            panelHome.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(75, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(110, 50);
+            label1.TabIndex = 1;
+            label1.Text = "Inicio";
+            label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 22);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(57, 57);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            panel3.Location = new Point(195, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(787, 556);
+            panel3.TabIndex = 1;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(12, 22);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(57, 48);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 0;
+            pictureBox5.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(75, 22);
+            label5.Name = "label5";
+            label5.Size = new Size(95, 31);
+            label5.TabIndex = 1;
+            label5.Text = "Clientes";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Indigo;
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(pictureBox5);
+            panel2.Location = new Point(0, 385);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(196, 87);
+            panel2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(982, 553);
+            Controls.Add(panel3);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panelConfig.ResumeLayout(false);
+            panelConfig.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            panelLocal.ResumeLayout(false);
+            panelLocal.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            panelLoja.ResumeLayout(false);
+            panelLoja.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panelClientes.ResumeLayout(false);
+            panelClientes.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panelHome.ResumeLayout(false);
+            panelHome.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panelHome;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Panel panelClientes;
+        private Label label2;
+        private PictureBox pictureBox2;
+        private Panel panelLoja;
+        private Label label3;
+        private PictureBox pictureBox3;
+        private Panel panelConfig;
+        private PictureBox pictureBox6;
+        private Panel panelLocal;
+        private Label label4;
+        private PictureBox pictureBox4;
+        private Label label6;
+        private Panel panel3;
+        private PictureBox pictureBox5;
+        private Panel panel2;
+        private Label label5;
+    }
+}
---
+++ FrmTeste/FrmTeste/Form1.cs
@@ -0,0 +1,30 @@
+namespace FrmTeste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
